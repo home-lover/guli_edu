a5cc0a2fb8f7db9d3cd2aa89b8aaac4de2c324e3
home-lover <964161300@qq.com>
2020-04-14 19:56:47 +0800

    first commit

---
+++ guli-microservice-edu/src/main/java/com/guli/edu/service/CourseService.java
@@ -0,0 +1,16 @@
+package com.guli.edu.service;
+
+import com.guli.edu.entity.Course;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ * 课程 服务类
+ * </p>
+ *
+ * @author Jinhong
+ * @since 2020-04-14
+ */
+public interface CourseService extends IService<Course> {
+
+}
